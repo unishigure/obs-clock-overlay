60c177c07223f0208864f8072510932474725a7b
unishigure <s.reiuniuni@gmail.com>
2023-09-05 00:20:57 +0900

    Add Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { Suspense } from "react";
+
 import DisplayDate from "./components/displayDate";
 import DisplayTime from "./components/displayTime";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col justify-center text-center bg-[rgba(39,39,39,0.7)] text-white w-80 h-40 rounded-lg">
-        <DisplayTime />
-        <DisplayDate />
-      </div>
-    </main>
+    <Suspense fallback={<Loading />}>
+      <main>
+        <div className="flex flex-col justify-center text-center bg-[rgba(39,39,39,0.7)] text-white w-80 h-40 rounded-lg">
+          <DisplayTime />
+          <DisplayDate />
+        </div>
+      </main>
+    </Suspense>
   );
+}
+
+function Loading() {
+  return <h2>Loading...</h2>;
 }
