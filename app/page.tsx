a5041b060d1d4283c1af1bdfa6be21899fb93650
unishigure <s.reiuniuni@gmail.com>
2023-09-05 00:55:23 +0900

    Revert "Add Suspense"
    This reverts commit 60c177c07223f0208864f8072510932474725a7b.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { Suspense } from "react";
-
 import DisplayDate from "./components/displayDate";
 import DisplayTime from "./components/displayTime";
 
 export default function Home() {
   return (
-    <Suspense fallback={<Loading />}>
-      <main>
-        <div className="flex flex-col justify-center text-center bg-[rgba(39,39,39,0.7)] text-white w-80 h-40 rounded-lg">
-          <DisplayTime />
-          <DisplayDate />
-        </div>
-      </main>
-    </Suspense>
+    <main>
+      <div className="flex flex-col justify-center text-center bg-[rgba(39,39,39,0.7)] text-white w-80 h-40 rounded-lg">
+        <DisplayTime />
+        <DisplayDate />
+      </div>
+    </main>
   );
-}
-
-function Loading() {
-  return <h2>Loading...</h2>;
 }
