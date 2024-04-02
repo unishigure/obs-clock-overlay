1f8210d22feae8d275874acb001834b6a4e75086
unishigure <uni@seanut.app>
2024-04-03 01:51:24 +0900

    Revert "Update component"
    This reverts commit 6826633d385990d314ef736068315558a13f22a2.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import DisplayDateTime from "./components/displayDateTime";
+import DisplayDate from "./components/displayDate";
+import DisplayTime from "./components/displayTime";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen bg-transparent">
       <div className="flex flex-col justify-center text-center bg-[rgba(39,39,39,0.7)] text-white w-80 h-40 rounded-lg">
-        <DisplayDateTime />
+        <DisplayTime />
+        <DisplayDate />
       </div>
     </main>
   );
